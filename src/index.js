1583bd3eacf8c54234b33223bc52627a0e02d726
chifengtsai <fred_tsai@cmoney.com.tw>
2022-04-06 01:32:07 +0800

    fix: 修正react版本差異問題

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import './assets/styles/meyerweb.css';
 import './index.css';
+import './assets/styles/img.css';
+import './assets/styles/bg.css';
+import './assets/styles/btn.css';
+import './assets/styles/text.css';
 import App from './App';
-import { store } from './app/store';
+import store from './store/index.js';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
 
 // If you want your app to work offline and load faster, you can change
