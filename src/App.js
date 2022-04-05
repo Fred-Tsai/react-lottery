8421dd7a1b292ce518c59af5e75cbbe777c7dabd
chifengtsai <fred_tsai@cmoney.com.tw>
2022-04-06 02:24:45 +0800

    feat: 修改取得使用者清單的處理位置

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-// import { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { setUserList } from './store/slice/userList';
 import { selectResult } from './store/slice/result';
 import './App.css';
 import Timer from './components/Timer';
 import List from './components/List';
 import Result from './components/Result';
+import api from './plugins/api';
+import defaultData from './assets/scripts/defaultUser';
 
 const App = () => {
+  const dispatch = useDispatch();
+  const getUserList = async () => {
+    const res = await api.user.getUser({
+      results: Math.floor(Math.random() * 20) + 11,
+      inc: 'gender,login,email,picture',
+    });
+    if (res.errorStatus) {
+      dispatch(setUserList(defaultData.results));
+      return;
+    }
+    dispatch(setUserList(res.results));
+  };
+  useEffect(() => {
+    getUserList();
+  }, []);
   const { result = {} } = useSelector(selectResult);
   return (
     <div className="app">
--- src/components/List.js
+++ src/components/List.js
@@ -1,28 +1,10 @@
-import { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { selectUserList, setUserList } from '../store/slice/userList';
+import { useSelector } from 'react-redux';
+import { selectUserList } from '../store/slice/userList';
 import User from './User';
-import api from '../plugins/api';
 import './List.css';
-import defaultData from '../assets/scripts/defaultUser';
 
 const List = () => {
   const { userList = [] } = useSelector(selectUserList);
-  const dispatch = useDispatch();
-  const getUserList = async () => {
-    const res = await api.user.getUser({
-      results: Math.floor(Math.random() * 20) + 11,
-      inc: 'gender,login,email,picture',
-    });
-    if (res.errorStatus) {
-      dispatch(setUserList(defaultData.results));
-      return;
-    }
-    dispatch(setUserList(res.results));
-  };
-  useEffect(() => {
-    getUserList();
-  }, []);
   return (
     <div className="list">
       <h3 className="list__title">參與抽獎名單</h3>
